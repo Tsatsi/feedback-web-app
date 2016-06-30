f438d7c84c047622002b6338528fbf83119dd37a
tsatsimahase <tsatsi.mahase@gmail.com>
2016-06-30 22:57:29 +0200

    test setup

---
+++ feedback.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Feedback extends React.Component {
+  render(){
+      return <div> This is coded in braam feedback app</div>
+  }
+}
+
+ReactDOM.render(<Feedback/>, document.getElementById('feedback'));
